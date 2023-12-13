37fd23d1f6ae511b021921b394ed2c0f00068a24
Tristan Livengood <tlivengood55@gmail.com>
2023-12-13 10:36:50 -0500

    Completed webpack configas well as database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,43 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+    // Add the content to the database
+    await store.add({ content });
+
+    // Complete the transaction
+    await tx.complete;
+    console.log('Content added to the database:', content);
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  }
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+
+    // Retrieve all content from the database
+    const allContent = await store.getAll();
+
+    // Complete the transaction
+    await tx.complete;
+    console.log('All content retrieved from the database:', allContent);
+
+    return allContent;
+  } catch (error) {
+    console.error('Error retrieving content from the database:', error);
+    return null;
+  }
+};
 
 initdb();
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,59 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generate HTML files for the entry points
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+        filename: 'index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        chunks: ['install'],
+        filename: 'install.html',
+      }),
+
+      // Configure WebpackPwaManifest plugin
+      new WebpackPwaManifest({
+        name: 'Text-Editor',
+        short_name: 'txtedtr',
+        description: 'Single-page text editor app built with Webpack',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+
+      // Configure InjectManifest plugin for Service Worker
+      new InjectManifest({
+        swSrc: './src/sw.js', // Path to your service worker file
+        swDest: 'sw.js', // Output path for the generated service worker file
+      }),
     ],
 
     module: {
       rules: [
-        
+        // Add CSS loaders
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        // Add Babel loader for JavaScript files
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
